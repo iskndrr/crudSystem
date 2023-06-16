2efd54418e0a43333adebaa23189c0a3b48a028a
iskndrr <mohhamed.a.saeed@gmail.com>
2023-06-16 15:00:57 +0300

    validation added

--- js/index.js
+++ js/index.js
@@ -3,27 +3,16 @@ var productDesc = document.getElementById("pDesc");
 var productCat = document.getElementById("pCat");
 var productPrice = document.getElementById("pPrice");
 var productList;
-if(localStorage.getItem("productList") == null){
-  productList = []
-
-}else{
+if (localStorage.getItem("productList") == null) {
+  productList = [];
+} else {
   productList = JSON.parse(localStorage.getItem("productList"));
-  displayProduct(productList)
+  displayProduct(productList);
 }
 
 console.log(productList);
 function getProducts() {
-  var product = {
-    pName: productName.value,
-    pDesc: productDesc.value,
-    pCat: productCat.value,
-    pPrice: productPrice.value,
-  };
-  productList.push(product);
-  localStorage.setItem("productList", JSON.stringify(productList));
-  console.log(productList);
-  displayProduct(productList);
-  clearList();
+  validation();
 }
 
 function displayProduct(product) {
@@ -32,7 +21,9 @@ function displayProduct(product) {
     Table += `
     <tr>
                 <th scope="row">${i + 1}</th>
-                <td>${product[i].newName ? product[i].newName : product[i].pName }</td>
+                <td>${
+                  product[i].newName ? product[i].newName : product[i].pName
+                }</td>
                 <td>${product[i].pDesc}</td>
                 <td>${product[i].pCat}</td>
                 <td>${product[i].pPrice}</td>
@@ -67,15 +58,85 @@ function deleteProduct(index) {
   displayProduct(productList);
 }
 
-
-function searchByName(term){
+function searchByName(term) {
   var foundedItems = [];
-for(var i =0; i < productList.length; i++){
-if(productList[i].pName.toLowerCase().includes(term.toLowerCase()) == true){
-  productList[i].newName = productList[i].pName.toLowerCase().replace(term.toLowerCase(),`<span class="text-danger fw-bold">${term}</span>`)
-foundedItems.push(productList[i])
-// alert("true")
+  for (var i = 0; i < productList.length; i++) {
+    if (
+      productList[i].pName.toLowerCase().includes(term.toLowerCase()) == true
+    ) {
+      productList[i].newName = productList[i].pName
+        .toLowerCase()
+        .replace(
+          term.toLowerCase(),
+          `<span class="text-danger fw-bold">${term}</span>`
+        );
+      foundedItems.push(productList[i]);
+      // alert("true")
+    }
+  }
+  displayProduct(foundedItems);
 }
+
+function validatName() {
+  var regex = /^[A-Z][a-z]{3,8}[1-9]{0,1}[0-9]{0,2}$/;
+  if (regex.test(productName.value) == true) {
+    document.getElementById("nameValidate").classList.add("d-none");
+    return true;
+  } else {
+    document.getElementById("nameValidate").classList.remove("d-none");
+    return false;
+  }
 }
-displayProduct(foundedItems)
+function validatDesc() {
+  var regex = /^[A-Z][a-z]{3,8}[1-9]{0,1}[0-9]{0,2}$/;
+  if (regex.test(productDesc.value) == true) {
+    document.getElementById("descValidate").classList.add("d-none");
+    return true;
+  } else {
+    document.getElementById("descValidate").classList.remove("d-none");
+    return false;
+  }
+}
+function validatCat() {
+  var regex = /^(Mobile|Tv|Laptop)$/;
+  if (regex.test(productCat.value) == true) {
+    document.getElementById("catValidate").classList.add("d-none");
+    return true;
+  } else {
+    document.getElementById("catValidate").classList.remove("d-none");
+    return false;
+  }
+}
+function validatPrice() {
+  var regex = /^[1-9][0-9]{1,3}$/;
+  if (regex.test(productPrice.value) == true) {
+    document.getElementById("priceValidate").classList.add("d-none");
+    return true;
+  } else {
+    document.getElementById("priceValidate").classList.remove("d-none");
+    return false;
+  }
+}
+
+function validation() {
+  if (
+    validatName() == true &&
+    validatDesc() == true &&
+    validatCat() == true &&
+    validatPrice() == true
+  ) {
+    var product = {
+      pName: productName.value,
+      pDesc: productDesc.value,
+      pCat: productCat.value,
+      pPrice: productPrice.value,
+    };
+
+    productList.push(product);
+    localStorage.setItem("productList", JSON.stringify(productList));
+    console.log(productList);
+    // alert("valid")
+    displayProduct(productList);
+    clearList();
+  }
 }
